740f070dd20be8987b690c3f7868098e51578819
prnv2003 <gadadepranav8@gmail.com>
2026-04-11 02:35:43 +0530

    Added a New Question!

---
+++ 0001-1000/Solution.java
@@ -0,0 +1,104 @@
+
+/*
+ * 51. N-Queens
+ */
+
+// import java.util.ArrayList;
+import java.util.*;
+
+public class Solution {
+
+    public static boolean isSafe(int row, int col, char[][] board) {
+        // Check horizontal left
+        for (int j = 0; j < col; j++) {
+            if (board[row][j] == 'Q') {
+                return false;
+            }
+        }
+        // Check vertical up
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // Check upper left diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // Check upper right diagonal
+        for (int i = row, j = col; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // Check lower left diagonal
+        for (int i = row, j = col; i < board.length && j >= 0; i++, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // Check lower right diagonal
+        for (int i = row, j = col; i < board.length && j < board.length; i++, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void saveBoard(char[][] board, List<List<String>> allBoards) {
+        List<String> newBoard = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            StringBuilder row = new StringBuilder();
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'Q') {
+                    row.append('Q');
+                } else {
+                    row.append('.');
+                }
+            }
+            newBoard.add(row.toString());
+        }
+        allBoards.add(newBoard);
+    }
+
+    public static void helper(char[][] board, List<List<String>> allBoards, int col) {
+        if (col == board.length) {
+            saveBoard(board, allBoards);
+            return;
+        }
+
+        for (int row = 0; row < board.length; row++) {
+            if (isSafe(row, col, board)) {
+                board[row][col] = 'Q';
+                helper(board, allBoards, col + 1); // Move to the next column
+                board[row][col] = '.'; // Backtrack
+            }
+        }
+    }
+
+    public static List<List<String>> solveNQueens(int n) {
+        List<List<String>> allBoards = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        helper(board, allBoards, 0);
+        return allBoards;
+    }
+
+    public static void main(String[] args) {
+        int n = 4; // Example input
+        List<List<String>> solutions = solveNQueens(n);
+        for (List<String> solution : solutions) {
+            for (String row : solution) {
+                System.out.println(row);
+            }
+            System.out.println();
+        }
+    }
+}
